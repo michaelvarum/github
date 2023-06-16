2543abcae7c8ccc9d1559917e66132fe139767e1
michael.varum <michaelvarum2@gmail.com>
2023-06-16 19:18:52 +0300

    musicplayer- the playlist run with audiofile

--- cpp/audio_test/audio_test.cpp
+++ cpp/audio_test/audio_test.cpp
@@ -1,7 +1,10 @@
 #include <SFML/Audio.hpp>
 #include <string>
 #include <iostream>
+#include <vector>
 #include <thread>
+#include <chrono>
+#include <atomic>
 #include <condition_variable>
 
 class AudioFile
@@ -10,45 +13,58 @@ public:
     AudioFile(const std::string& filename);
     ~AudioFile();
     bool load();
-    void play();
-    void stop();
-    const sf::Sound& GetSound();
+    void Play();
+    void Stop();
+    const sf::Sound& GetSound() const;
+    bool IsPlay() const;
 
 private:
     std::string m_filename;
-    sf::Sound m_sound;
     sf::SoundBuffer m_soundBuffer;
+    sf::Sound m_sound;
+    std::atomic<bool> m_isPlay;
 };
 
 AudioFile::AudioFile(const std::string& filename)
-    : m_filename(filename)
-{
-}
+    : m_filename(filename), m_isPlay(false)
+{}
 
 AudioFile::~AudioFile()
-{
-}
+{}
 
 bool AudioFile::load()
 {
     return m_soundBuffer.loadFromFile(m_filename);
 }
 
-void AudioFile::play()
+void AudioFile::Play()
 {
-    m_sound.setBuffer(m_soundBuffer);
+    m_sound.setBuffer(m_soundBuffer); // Set the sound buffer before playing
     m_sound.play();
+    m_isPlay = true;
 }
 
-void AudioFile::stop()
+void AudioFile::Stop()
 {
-    m_sound.stop();
+    m_sound.pause();
+    m_isPlay = false;
 }
 
-const sf::Sound& AudioFile::GetSound()
+
+const sf::Sound& AudioFile::GetSound() const
 {
     return m_sound;
 }
+
+bool AudioFile::IsPlay() const
+{
+    return m_isPlay;
+}
+//-------------------------------------------------------------------------------//
+
+
+
+
 
 
 class PlayList
@@ -56,35 +72,46 @@ class PlayList
 public:
     PlayList();
     ~PlayList();
-    void AddSong(const std::string& filename);
+    void AddAudioFile(const std::string& filename);
+    void Run();
     void Play();
     void Stop();
+    void Next();
+    void Prev();
+    bool IsPlay() const;
 
 private:
     std::vector<AudioFile*> m_audioFiles;
-    int m_currentSongIndex;
-    std::thread m_playbackThread;
-    std::condition_variable m_playbackCV;
-    std::mutex m_playbackMutex;
-    bool m_stopPlayback;
+    AudioFile* m_currAF;      //AF represent AudioFile
+    std::atomic<size_t> m_currIndxAF;
+    std::atomic<bool> m_isPlay;
+    std::condition_variable m_conditionVariable;
+    std::mutex m_mutex;
+    std::thread m_thread;
 };
 
 PlayList::PlayList()
-    : m_audioFiles(), m_currentSongIndex(0), m_playbackThread(), m_stopPlayback(false)
+: m_audioFiles(), m_currAF(nullptr), m_currIndxAF(0), m_isPlay(false)
 {
+    // Start the thread
+    m_thread = std::thread(&PlayList::Run, this);
 }
 
 PlayList::~PlayList()
 {
+    // Stop the playback and wait for the thread to finish
     Stop();
+    if (m_thread.joinable())
+        m_thread.join();
 
+    // Clean up the audio files
     for (AudioFile* audioFile : m_audioFiles)
     {
         delete audioFile;
     }
 }
 
-void PlayList::AddSong(const std::string& filename)
+void PlayList::AddAudioFile(const std::string& filename)
 {
     AudioFile* audioFile = new AudioFile(filename);
     if (audioFile->load())
@@ -98,67 +125,139 @@ void PlayList::AddSong(const std::string& filename)
     }
 }
 
-void PlayList::Play()
+void PlayList::Run()
 {
-    m_playbackThread = std::thread([this]() 
+    std::unique_lock<std::mutex> lock(m_mutex);
+    while (m_currIndxAF < m_audioFiles.size())
     {
-        for (m_currentSongIndex = 0; m_currentSongIndex < m_audioFiles.size(); ++m_currentSongIndex)
+        m_currAF = m_audioFiles[m_currIndxAF];
+        m_currAF->Play();
+
+        const sf::Sound* sound = &(m_currAF->GetSound());
+        while (sound->getStatus() == sf::Sound::Playing)
         {
-            std::lock_guard<std::mutex> lock(m_playbackMutex);
-            if (m_stopPlayback)
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            std::unique_lock<std::mutex> lock(m_mutex);
+            if (!IsPlay())
             {
-                break;
-            }
-            m_playbackMutex.unlock(); // Unlock the mutex
-
-            AudioFile* audioFile = m_audioFiles[m_currentSongIndex];
-            audioFile->play();
-
-            const sf::Sound* sound = &(audioFile->GetSound());
-            while (sound->getStatus() == sf::Sound::Playing)
-            {
-                std::lock_guard<std::mutex> lock(m_playbackMutex);
-                if (m_stopPlayback)
-                {
-                    audioFile->stop();
-                    break;
-                }
+                m_conditionVariable.wait(lock);
             }
         }
+        ++m_currIndxAF;
+    }
+}
 
-        std::lock_guard<std::mutex> lock(m_playbackMutex);
-        m_stopPlayback = true;
-        m_playbackCV.notify_all();
-    });
+void PlayList::Play()
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (!m_isPlay && m_currIndxAF < m_audioFiles.size())
+    {
+        m_currAF = m_audioFiles[m_currIndxAF];
+        m_currAF->Play();
+        m_isPlay = true;
+        m_conditionVariable.notify_one();
+    }
 }
 
 void PlayList::Stop()
 {
-    m_playbackMutex.lock(); // Lock the mutex for shared data access
-    m_stopPlayback = true; // Set the stop flag
-    m_playbackMutex.unlock(); // Unlock the mutex
-
-    m_playbackCV.notify_all();
-
-    if (m_playbackThread.joinable())
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (m_isPlay)
     {
-        m_playbackThread.join();
+        m_currAF->Stop();
+        m_isPlay = false;
+        m_conditionVariable.notify_one();
     }
 }
+
+void PlayList::Next()
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (m_currIndxAF < m_audioFiles.size() - 1)
+    {
+        bool wasPlay = IsPlay();
+        m_currAF->Stop();
+        ++m_currIndxAF;
+        m_currAF = m_audioFiles[m_currIndxAF];
+        if(wasPlay)
+        {
+            m_isPlay = true;
+            m_currAF->Play();
+            m_conditionVariable.notify_one();
+        }
+    }
+}
+
+void PlayList::Prev()
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (m_currIndxAF > 0)
+    {
+        bool wasPlay = IsPlay();
+        m_currAF->Stop();
+        --m_currIndxAF;
+        m_currAF = m_audioFiles[m_currIndxAF];
+        if(wasPlay)
+        {
+            m_isPlay = true;
+            m_currAF->Play();
+            m_conditionVariable.notify_one();
+        }
+    }
+}
+
+
+bool PlayList::IsPlay() const
+{
+    return m_isPlay;
+}
+
+
+
+//-------------------------------------------------------------------------------------------------------
+
+
+class MusicPlayer
+{
+public:
+    MusicPlayer();
+    void AddPlayList();
+    void RemovePlayList();
+    void AddSong(string plName, string songName);
+    void RemovePlayListSong(string plName, string songName);
+    void Run();
+    void Play();
+    void Stop();
+    void NextSong();
+    //void NextPlayList();
+
+private:
+    map<string, PlayList> m_playLists;
+    Song* m_currSong;
+    PlayList* m_currPlayList;
+    UserInterface* m_interface;
+};
+
+
+
+
+
+
 
 int main()
 {
     PlayList playlist;
-    playlist.AddSong("song1.wav");
-    playlist.AddSong("song2.wav");
+    playlist.AddAudioFile("song1.wav");
+    playlist.AddAudioFile("song2.wav");
 
+    // Start the playlist
     playlist.Play();
-
-    // Wait for user input to stop the playlist
-    std::cout << "Press enter to stop the playlist...";
-    std::cin.ignore();
-
-    playlist.Stop();
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    playlist.Next();
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    playlist.Prev();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    //playlist.Stop();
 
     return 0;
 }
